a9618b859db0c9e2006fdb2c49041ffede4a816b
ra2dd <radoslawpryw@gmail.com>
2023-10-09 21:48:53 +0200

    add address checking

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,29 @@
+import * as web3 from '@solana/web3.js'
 import type { NextPage } from 'next'
 import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
 
 const Home: NextPage = () => {
-  const [balance, setBalance] = useState(0)
-  const [address, setAddress] = useState('')
+  const [balance, setBalance] = useState(0);
+  const [address, setAddress] = useState('');
+  const [executable, setExecutable] = useState('');
 
-  const addressSubmittedHandler = (address: string) => {
-    setAddress(address)
-    setBalance(1000)
-  }
+  const addressSubmittedHandler = async (address: string) => {
+    try {
+      setAddress(address)
+      const key = new web3.PublicKey(address)
+      const connection = new web3.Connection(web3.clusterApiUrl('devnet'));
+      const balance = await connection.getBalance(key);
+      setBalance(balance / web3.LAMPORTS_PER_SOL);
+      const info = await connection.getAccountInfo(key)
+      info?.executable == true ? setExecutable('yes') : setExecutable('no');
+    } catch (error) {
+      setAddress("");
+      setBalance(0);
+      alert(error);
+    }
+  };
 
   return (
     <div className={styles.App}>
@@ -21,6 +34,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Executable: ${executable}`}</p>
       </header>
     </div>
   )
